e9e3b6cded47aabbb3375ee7df1e7358f0126a84
rohit-mapakshi97 <rohitmapakshi@gmail.com>
2024-03-04 17:29:59 -0800

    Linear SVC

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 from collections import OrderedDict
-from typing import Dict, Tuple
+from typing import Dict
 from flwr.common import NDArrays, Scalar
 
 import torch
@@ -12,7 +12,7 @@ from attacks import label_flipping_attack, targeted_label_flipping_attack, gan_a
 from dataset import get_data_numpy
 
 from sklearn.linear_model import LogisticRegression
-# from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.metrics import log_loss, precision_score, recall_score, f1_score, confusion_matrix
 import numpy as np
 import warnings
@@ -66,6 +66,20 @@ def generate_client_fn(traindataset_list: List[Dataset], valdataset_list: List[D
             num_classes=num_classes
         )
 
+    def client_fn_lsvc(cid: str):
+        # This function will be called internally by the VirtualClientEngine
+        # Each time the cid-th client is told to participate in the FL
+        # simulation (whether it is for doing fit() or evaluate())
+
+        # Returns a normal FLowerClient that will use the cid-th train/val
+        # dataloaders as it's local data.
+        return FlowerClientLSVC(
+            traindataset=traindataset_list[int(cid)],
+            valdataset=valdataset_list[int(cid)],
+            num_classes=num_classes,
+            num_features=28 * 28  # TODO configurable?
+        )
+
     # Control logic for other models
     # return the function to spawn client
     if model == "SCNN":
@@ -74,6 +88,8 @@ def generate_client_fn(traindataset_list: List[Dataset], valdataset_list: List[D
         return client_fn_lgr
     elif model == "MLP":
         return client_fn_mlp
+    elif model == "LSVC":
+        return client_fn_lsvc
     else:
         return None
 
@@ -345,6 +361,115 @@ class FlowerClientMLP(fl.client.NumPyClient):
 
         return float(loss), len(valloader), {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1,
                                              "confusion_matrix": conf_matrix}
+
+
+class FlowerClientLSVC(fl.client.NumPyClient):
+    '''Define a Flower Client'''
+
+    def __init__(self, traindataset: Dataset, valdataset: Dataset, num_classes: int, num_features) -> None:
+        super().__init__()
+
+        # the dataloaders that point to the data associated to this client
+        self.traindataset = traindataset
+        self.valdataset = valdataset
+
+        # a model that is randomly initialised at first
+        self.model = LinearSVC(dual=False)
+        self.num_classes = num_classes
+        self.num_features = num_features
+
+        self.model.classes_ = np.array([i for i in range(num_classes)])
+        self.model.coef_ = np.zeros((num_classes, self.num_features))
+        if self.model.fit_intercept:
+            self.model.intercept_ = np.zeros((num_classes,))
+
+    def set_parameters(self, parameters):
+        """Receive parameters and apply them to the local model."""
+        """Sets the parameters of a sklean LogisticRegression model."""
+        self.model.coef_ = parameters[0]
+        if self.model.fit_intercept:
+            self.model.intercept_ = parameters[1]
+
+    def get_parameters(self, config: Dict[str, Scalar]):
+        """Extract model parameters and return them as a list of numpy arrays."""
+        if self.model.fit_intercept:
+            params = [self.model.coef_, self.model.intercept_]
+        else:
+            params = [self.model.coef_, ]
+        return params
+
+    def fit(self, parameters, config):
+        """Train model received by the server (parameters) using the data.
+
+        that belongs to this client. Then, send it back to the server.
+        """
+        # Poison the dataset if the client is malicious
+        self.traindataset = applyAttacks(self.traindataset, config, model="LGR")
+
+        # copy parameters sent by the server into client's local model
+        self.set_parameters(parameters)
+
+        # fetch elements in the config sent by the server. Note that having a config
+        # sent by the server each time a client needs to participate is a simple but
+        # powerful mechanism to adjust these hyperparameters during the FL process. For
+        # example, maybe you want clients to reduce their LR after a number of FL rounds.
+        # or you want clients to do more local epochs at later stages in the simulation
+        # you can control these by customising what you pass to `on_fit_config_fn` when
+        # defining your strategy.
+        penalty = config["penalty"]
+        # warm_start = config["warm_start"]
+        epochs = config["local_epochs"]
+        c = config["C"]
+
+        self.model.penalty = penalty
+        # self.model.warm_start = warm_start
+        self.model.max_iter = epochs
+        self.model.C = c
+
+        # do local training. This function is identical to what you might
+        # have used before in non-FL projects. For more advance FL implementation
+        # you might want to tweak it but overall, from a client perspective the "local
+        # training" can be seen as a form of "centralised training" given a pre-trained
+        # model (i.e. the model received from the server)
+        trainloader = DataLoader(self.traindataset)
+        # Convert to numpy data
+        X_train, y_train = get_data_numpy(trainloader)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.model.fit(X_train, y_train)
+            # print(f"Training finished for round {config['server_round']}")
+
+        # Flower clients need to return three arguments: the updated model, the number
+        # of examples in the client (although this depends a bit on your choice of aggregation
+        # strategy), and a dictionary of metrics (here you can add any additional data, but these
+        # are ideally small data structures)
+        return self.get_parameters({}), len(X_train), {}
+
+    def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
+        self.set_parameters(parameters)
+
+        valloader = DataLoader(self.valdataset)
+        X_test, y_test = get_data_numpy(valloader)
+
+        descision_scores = self.model.decision_function(X_test)
+        y_pred_prob = 1 / (1 + np.exp(-descision_scores))
+
+        y_pred = self.model.predict(X_test)
+
+        loss = log_loss(y_test, y_pred_prob)
+        accuracy = self.model.score(X_test, y_test)
+
+        # Precision, Recall, F1_score
+        precision = precision_score(y_test, y_pred, average='weighted')
+        recall = recall_score(y_test, y_pred, average='weighted')
+        f1 = f1_score(y_test, y_pred, average='weighted')
+
+        # Confusion Matrix
+        conf_matrix = confusion_matrix(y_test, y_pred, labels=list(range(10)))
+
+        return float(loss), len(X_test), {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1,
+                                          "confusion_matrix": conf_matrix}
 
 
 def applyAttacks(trainset: Dataset, config, model: str = None) -> Dataset:
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 from omegaconf import DictConfig
 
 import torch
+from sklearn.svm import LinearSVC
 
 from model import SimpleCNN, MLP, test
 from torch.utils.data import Dataset, DataLoader
@@ -52,6 +53,24 @@ def get_on_fit_config(config: DictConfig, model_name: str):
             "is_malicious": False
         }
 
+    def fit_config_fn_lsvc(server_round: int):
+        # This function will be executed by the strategy in its
+        # `configure_fit()` method.
+
+        # Here we are returning the same config on each round but
+        # here you might use the `server_round` input argument to
+        # adapt over time these settings so clients. For example, you
+        # might want clients to use a different learning rate at later
+        # stages in the FL process (e.g. smaller lr after N rounds)
+
+        return {
+            "penalty": config.penalty,
+            # "warm_start": config.warm_start,
+            "local_epochs": config.local_epochs,
+            "C": config.C,
+            "is_malicious": False
+        }
+
     def fit_config_fn_mlp(server_round: int):
         # This function will be executed by the strategy in its
         # `configure_fit()` method.
@@ -75,6 +94,8 @@ def get_on_fit_config(config: DictConfig, model_name: str):
         return fit_config_fn_lgr
     elif model_name == "MLP":
         return fit_config_fn_mlp
+    elif model_name == "LSVC":
+        return fit_config_fn_lsvc
     else:
         return None
 
@@ -204,11 +225,66 @@ def get_evaluate_fn(num_classes: int, testset: Dataset, model_name: str):
         return loss, {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1,
                       "confusion_matrix": conf_matrix}
 
+    def evaluate_fn_lscv(server_round: int, parameters, config):
+        # This function is called by the strategy's `evaluate()` method
+        # and receives as input arguments the current round number and the
+        # parameters of the global model.
+        # this function takes these parameters and evaluates the global model
+        # on a evaluation / test dataset.
+
+        model = LinearSVC(dual=False)
+
+        model.classes_ = np.array([i for i in range(num_classes)])
+        model.coef_ = parameters[0]
+        if model.fit_intercept:
+            model.intercept_ = parameters[1]
+
+        # Here we evaluate the global model on the test set. Recall that in more
+        # realistic settings you'd only do this at the end of your FL experiment
+        # you can use the `server_round` input argument to determine if this is the
+        # last round. If it's not, then preferably use a global validation set.
+
+        # We leave the test set intact (i.e. we don't partition it)
+        # This test set will be left on the server side and we'll be used to evaluate the
+        # performance of the global model after each round.
+        # Please note that a more realistic setting would instead use a validation set on the server for
+        # this purpose and only use the testset after the final round.
+        # Also, in some settings (specially outside simulation) it might not be feasible to construct a validation
+        # set on the server side, therefore evaluating the global model can only be done by the clients. (see the comment
+        # in main.py above the strategy definition for more details on this)
+        testloader = DataLoader(testset)
+        X_test, y_test = get_data_numpy(testloader)
+
+
+        descision_scores = model.decision_function(X_test)
+        y_pred_prob = 1 / (1 + np.exp(-descision_scores))
+
+        y_pred = model.predict(X_test)
+
+        # Loss
+        loss = log_loss(y_test, y_pred_prob)
+        # Accuracy
+        accuracy = model.score(X_test, y_test)
+        # Precision, Recall, F1_score
+        precision = precision_score(y_test, y_pred, average='weighted')
+        recall = recall_score(y_test, y_pred, average='weighted')
+        f1 = f1_score(y_test, y_pred, average='weighted')
+
+        # Confusion Matrix
+        conf_matrix = confusion_matrix(y_test, y_pred, labels=list(range(10)))
+
+        # Report the loss and any other metric (inside a dictionary). In this case
+        # we report the global test accuracy.
+        return loss, {"accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1,
+                      "confusion_matrix": conf_matrix}
+
     if model_name == "SCNN":
         return evaluate_fn_scnn
     elif model_name == "LGR":
         return evaluate_fn_lgr
     elif model_name == "MLP":
         return evaluate_fn_mlp
+    elif model_name == "LSVC":
+        return evaluate_fn_lscv
     else:
         return None
